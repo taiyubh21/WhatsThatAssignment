b243ca1e0b499cb01bc441ba8d96abe15563f8b3
Mohammad Taiyub Hussain <taiyubhussain@gmail.com>
2023-02-13 14:19:47 +0000

    Created a react native project
    - New react native project with Hello World for the start
    - To build WhatsThat app upon

---
+++ WhatsThatApp/WhatsThatapp/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class WhatsThatapp extends Component {
+  render(){
+
+    return (
+        <View>
+          <Text>Hello World</Text>
+        </View>
+    );
+  }
+}
+
+export default WhatsThatapp
+

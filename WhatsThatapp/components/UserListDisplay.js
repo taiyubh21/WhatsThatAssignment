eaceb1b569236e2c86fdd9aa494f59d53ea624f6
Mohammad Taiyub Hussain <taiyubhussain@gmail.com>
2023-03-12 00:17:22 +0000

    Update UserListDisplay.js
    - Added a simple if statement checking if current user ID was equal to the user ID in the data and if it is to not output those account details
    - Had an error where this wasn't working but all I needed to do was parse the user ID from async storage as an integer
    - Next on the agenda is to try and implement a textbox which will allow users to search through the user list and also add some buttons to the users which will allow the current logged in user to add them to their contacts

--- WhatsThatapp/components/UserListDisplay.js
+++ WhatsThatapp/components/UserListDisplay.js
@@ -7,17 +7,24 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
  class UserListDisplay extends Component {
     constructor(props){
         super(props);
+        // Initialising states
         this.state = {
+            // Used for loading icon
             isLoading: true,
+            // For array of user data
             userListData: [],
+            // For holding the ID of current user
             currentUserId: null
         };
         this.setCurrentUserId();
     }
 
+    // Getting the current ID from async storage
+    // Setting it to the new state
+    // Making sure its an integer
     async setCurrentUserId() {
         const userId = await AsyncStorage.getItem("whatsthat_user_id");
-        this.setState({ currentUserId: userId });
+        this.setState({ currentUserId: parseInt(userId) });
       }
 
     async getData(){
@@ -26,9 +33,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
             headers: {
               "X-Authorization": await AsyncStorage.getItem("whatsthat_session_token")
             }
-          })          
+          })    
+
         .then((response) => response.json())
         .then((responseJson) => {
+            // Updating the userListData state with the retrieved data
             this.setState({
                 isLoading: false,
                 userListData: responseJson
@@ -44,6 +53,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
     }
 
   render(){
+    // If data is still being fetched return a loading spinner
     if(this.state.isLoading){
         return(
             <View>
@@ -54,15 +64,30 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
         return(
             <View>
                 <FlatList
-                    data={this.state.userListData.filter((user) => user.user_id !== this.state.currentUserId)}                  
-                    renderItem= {({item}) => (
-                        <View key={item.id}>
-                            <Text>{item.given_name + ' ' + item.family_name}</Text> 
-                            <Text>{item.email}</Text> 
-                            <Text>{' '}</Text>
-                        </View>
-                    )}
-                    keyExtractor={(item) => item.user_id.toString()}
+                    data={this.state.userListData}              
+                    renderItem= {({item}) => {
+                        // Console code for checking the IDs of the data and the current ID of the account logged in
+                        // For debugging
+                        console.log("item user id:", item.user_id);
+                        console.log("current user id:", this.state.currentUserId);
+                        // If the user in the data has the same ID as the current user logged in then
+                        // don't output their account
+                        if(item.user_id !== this.state.currentUserId){
+                            return(
+                                <View>
+                                    {/* Concatenating first name and last name together */}
+                                    <Text>{item.given_name + ' ' + item.family_name}</Text> 
+                                    <Text>{item.email}</Text> 
+                                    {/* Empty line inbetween account details*/}
+                                    <Text>{' '}</Text>
+                                </View>
+                                );
+                            // If the user is the current user logged in return nothing
+                            } else{
+                                return null;
+                            }
+                    }}
+                    keyExtractor={(item) => item.user_id}
                 />
             </View>
         );
